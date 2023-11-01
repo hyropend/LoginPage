600cf24e19dfa0d2f24d795e53846c682c87d389
Buğrahan Yaşar <87487330+hyropend@users.noreply.github.com>
2023-11-01 23:06:00 +0300

    Add files via upload

---
+++ LoginPage/LoginS/LoginPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./LoginPage.css";
+
+import user_icon from "../Assets/person.png";
+import password_icon from "../Assets/password.png";
+import email_icon from "../Assets/email.png";
+
+const LoginPage = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="E-mail" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+      <div className="forgot-password">
+        Forgot Password? <span>Click Here</span>
+      </div>
+      <div className="submit-container">
+        <div className="submit">Sign up</div>
+        <div className="submit">Login</div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
